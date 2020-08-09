282a7de7f86379ba7a4913a49807bd06054efc86
ZmjDns <292570927@qq.com>
2020-08-09 23:03:44 +0800

    添加数据测试lateinit var判空

--- app/src/main/java/com/zmj/androidfirstline/ui/ChatAct.kt
+++ app/src/main/java/com/zmj/androidfirstline/ui/ChatAct.kt
@@ -5,7 +5,10 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.zmj.androidfirstline.R
-import com.zmj.permissionx.PermissionX
+import com.zmj.androidfirstline.model.Msg
+import com.zmj.androidfirstline.ui.adapter.ChatAdapter
+//import com.zmj.permissionx.PermissionX
+import kotlinx.android.synthetic.main.act_chat.*
 
 /**
  * Author : Zmj
@@ -16,22 +19,50 @@ import com.zmj.permissionx.PermissionX
  */
 class ChatAct: AppCompatActivity() {
 
+    private val msgList = ArrayList<Msg>()
+    private lateinit var adapter: ChatAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.act_chat)
 
-        PermissionX.request(this,Manifest.permission.WRITE_EXTERNAL_STORAGE,
+        supportActionBar?.hide()
+
+        /*PermissionX.request(this,Manifest.permission.WRITE_EXTERNAL_STORAGE,
                                         Manifest.permission.READ_EXTERNAL_STORAGE){allGranted,deniedList ->
             if (allGranted){
                 Toast.makeText(this,"申请权限成功",Toast.LENGTH_SHORT).show()
             }else{
                 Toast.makeText(this,"You denied $deniedList",Toast.LENGTH_SHORT).show()
             }
+        }*/
+        initMsg()
+
+        if (!::adapter.isInitialized){
+            adapter = ChatAdapter(msgList)
+        }
+
+        recycler.adapter = adapter
+
+        btn_send.setOnClickListener {
+            val content = et_text.text.toString()
+            if (content.isNotEmpty()){
+                val msg = Msg(content,Msg.TYPE_SEND)
+                msgList.add(msg)
+                adapter.notifyItemInserted(msgList.size - 1)    //有新消息
+                //刷新recyclerView中的显示
+                recycler.scrollToPosition(msgList.size - 1)
+                et_text.setText("")
+            }
         }
     }
 
-
+    private fun initMsg(){
+        msgList.add(Msg("Hello guy",Msg.TYPE_RECEIVED))
+        msgList.add(Msg("Hello who is that?",Msg.TYPE_SEND))
+        msgList.add(Msg("This is Tom,nice talking to you!",Msg.TYPE_RECEIVED))
+    }
 
 
 }
