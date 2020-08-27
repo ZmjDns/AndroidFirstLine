6177b54f0cca3d8f004321b45efc9459648f2bd6
ZmjDns <292570927@qq.com>
2020-08-27 15:32:08 +0800

    coroutine 再理解

--- app/src/main/java/com/zmj/androidfirstline/coroutine/conroutine.kt
+++ app/src/main/java/com/zmj/androidfirstline/coroutine/conroutine.kt
@@ -1,6 +1,9 @@
 package com.zmj.androidfirstline.coroutine
 
 import kotlinx.coroutines.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import kotlin.coroutines.resume
 import kotlin.coroutines.resumeWithException
 import kotlin.coroutines.suspendCoroutine
@@ -29,6 +32,7 @@ fun main() {
         val start = System.currentTimeMillis()
         val result = async { delay(1000); 5 + 5 }
         val result2 = async { delay(1000); 5+5 }
+        //println("result: ${result + result2}")
         println("result: ${result.await() + result2.await()}")
         val end = System.currentTimeMillis()
         println("const time: ${end - start}")
@@ -54,4 +58,24 @@ interface HttpCallBackListener{
     fun onFinish(resp: String)
 
     fun onError(e: Exception)
+}
+
+//终极简化
+//Call<T>的扩展函数
+suspend fun <T>Call<T>.await(): T{
+    return suspendCoroutine {continuation ->
+        enqueue(object : Callback<T>{
+            override fun onFailure(call: Call<T>, t: Throwable) {
+                continuation.resumeWithException(t)
+            }
+            override fun onResponse(call: Call<T>, response: Response<T>) {
+                val body = response.body()
+                if (body != null){
+                    continuation.resume(body)
+                }else{
+                    continuation.resumeWithException(RuntimeException("response is null"))
+                }
+            }
+        })
+    }
 }
